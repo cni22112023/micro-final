995326a91eadb6c16087a32f6ac0b6242016998e
cni22112023 <151912403+cni22112023@users.noreply.github.com>
2024-12-03 14:36:49 +0000

    demo

---
+++ demo-config-registry/src/main/java/tn/cni/demo_config_registry/DemoConfigRegistryApplication.java
@@ -0,0 +1,13 @@
+package tn.cni.demo_config_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConfigRegistryApplication.class, args);
+	}
+
+}
---
+++ demo-config-registry/src/test/java/tn/cni/demo_config_registry/DemoConfigRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.cni.demo_config_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
