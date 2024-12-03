7f1a9e1990a8e835586a57696ee7d8cb45b8bc42
cni22112023 <151912403+cni22112023@users.noreply.github.com>
2024-12-03 18:17:52 +0000

    test

---
+++ micro-one/src/main/java/tn/cni/micro_one/MicroOneApplication.java
@@ -0,0 +1,13 @@
+package tn.cni.micro_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroOneApplication.class, args);
+	}
+
+}
---
+++ micro-one/src/test/java/tn/cni/micro_one/MicroOneApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.cni.micro_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
