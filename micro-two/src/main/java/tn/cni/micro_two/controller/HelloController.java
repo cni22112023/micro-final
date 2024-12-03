a682a69d0ed0cbacad423013e4bce6123e3c2e9e
cni22112023 <151912403+cni22112023@users.noreply.github.com>
2024-12-03 18:35:10 +0000

    test

--- micro-two/src/main/java/tn/cni/micro_two/controller/HelloController.java
+++ micro-two/src/main/java/tn/cni/micro_two/controller/HelloController.java
@@ -1,4 +1,4 @@
-package tn.cni.micro_two;
+package tn.cni.micro_two.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
